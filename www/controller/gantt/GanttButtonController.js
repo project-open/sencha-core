b863719b94e80154a2434c87fcd97898928f9587
Frank Bergmann <frank.bergmann@project-open.com>
2014-03-18 17:51:42 +0000

    - GanttEditor:   Fixed resizing

--- www/controller/gantt/GanttButtonController.js
+++ www/controller/gantt/GanttButtonController.js
@@ -13,17 +13,24 @@
 Ext.define('PO.controller.gantt.GanttButtonController', {
     extend: 'Ext.app.Controller',
 
+    // Variables
     debug: false,
+    'renderDiv': null,
+    'ganttEditor': null,
+    'ganttButtonController': null,
+    'ganttTreePanel': null,
+    'ganttDrawComponent': null,
+    
 
     refs: [
-	{ ref: 'ganttTreePanel', selector: '#ganttTreePanel' }
+        { ref: 'ganttTreePanel', selector: '#ganttTreePanel' }
     ],
     
     init: function() {
-	if (this.debug) { console.log('PO.controller.gantt.GanttButtonController: init'); }
+        if (this.debug) { console.log('PO.controller.gantt.GanttButtonController: init'); }
 
-	var panel = this.getGanttTreePanel();
-	this.control({
+        var panel = this.getGanttTreePanel();
+        this.control({
             '#buttonLoad': { click: this.onButtonLoad },
             '#buttonSave': { click: this.onButton },
             '#buttonAdd': { click: { fn: panel.onButtonAdd, scope: panel }},
@@ -36,35 +43,35 @@ Ext.define('PO.controller.gantt.GanttButtonController', {
             '#buttonZoomIn': { click: this.onButton },
             '#buttonZoomOut': { click: this.onButton },
             '#buttonSettings': { click: this.onButton },
-	    scope: panel
+            scope: panel
         });
 
-	// Listen to changes in the selction model in order to enable/disable the "delete" button.
-	panel.on('selectionchange', this.onTreePanelSelectionChange, this);
+        // Listen to changes in the selction model in order to enable/disable the "delete" button.
+        panel.on('selectionchange', this.onTreePanelSelectionChange, this);
 
-	// Listen to a click into the empty space below the tree in order to add a new task
-	panel.on('containerclick', panel.onContainerClick, panel);
+        // Listen to a click into the empty space below the tree in order to add a new task
+        panel.on('containerclick', panel.onContainerClick, panel);
 
-	// Listen to special keys
-	panel.on('cellkeydown', this.onCellKeyDown, panel);
-	panel.on('beforecellkeydown', this.onBeforeCellKeyDown, panel);
-	return this;
+        // Listen to special keys
+        panel.on('cellkeydown', this.onCellKeyDown, panel);
+        panel.on('beforecellkeydown', this.onBeforeCellKeyDown, panel);
+        return this;
     },
 
     onButtonLoad: function() {
-	console.log('ButtonLoad');
+        console.log('ButtonLoad');
     },
 
     onButtonSave: function() {
-	console.log('ButtonSave');
+        console.log('ButtonSave');
     },
 
     /**
      * Control the enabled/disabled status of the (-) (Delete) button
      */
     onTreePanelSelectionChange: function(view, records) {
-	if (this.debug) { console.log('GanttButtonController.onTreePanelSelectionChange'); }
-	var buttonDelete = Ext.getCmp('buttonDelete');
+        if (this.debug) { console.log('GanttButtonController.onTreePanelSelectionChange'); }
+        var buttonDelete = Ext.getCmp('buttonDelete');
 
         if (1 == records.length) {            // Exactly one record enabled
             var record = records[0];
@@ -78,44 +85,89 @@ Ext.define('PO.controller.gantt.GanttButtonController', {
      * Disable default tree key actions
      */
     onBeforeCellKeyDown: function(me, htmlTd, cellIndex, record, htmlTr, rowIndex, e, eOpts ) {
-	var keyCode = e.getKey();
-	var keyCtrl = e.ctrlKey;
-	if (this.debug) { console.log('GanttButtonController.onBeforeCellKeyDown: code='+keyCode+', ctrl='+keyCtrl); }
-	var panel = this;
-	switch (keyCode) {
-	case 8: 				// backspace 8
-	    panel.onButtonDelete();
-	    break;
-	case 37: 				// cursor left
-	    if (keyCtrl) {
-		panel.onButtonReduceIndent();
-		return false;                   // Disable default action (fold tree)
-	    }
-	    break;
-	case 39: 				// cursor right
-	    if (keyCtrl) {
-		panel.onButtonIncreaseIndent();
-		return false;                   // Disable default action (unfold tree)
-	    }
-	    break;
-	case 45: 				// insert 45
-	    panel.onButtonAdd();
-	    break;
-	case 46: 				// delete 46
-	    panel.onButtonDelete();
-	    break;
-	}
+        var keyCode = e.getKey();
+        var keyCtrl = e.ctrlKey;
+        if (this.debug) { console.log('GanttButtonController.onBeforeCellKeyDown: code='+keyCode+', ctrl='+keyCtrl); }
+        var panel = this;
+        switch (keyCode) {
+        case 8: 				// backspace 8
+            panel.onButtonDelete();
+            break;
+        case 37: 				// cursor left
+            if (keyCtrl) {
+        	panel.onButtonReduceIndent();
+        	return false;                   // Disable default action (fold tree)
+            }
+            break;
+        case 39: 				// cursor right
+            if (keyCtrl) {
+        	panel.onButtonIncreaseIndent();
+        	return false;                   // Disable default action (unfold tree)
+            }
+            break;
+        case 45: 				// insert 45
+            panel.onButtonAdd();
+            break;
+        case 46: 				// delete 46
+            panel.onButtonDelete();
+            break;
+        }
 
-	return true;                            // Enable default TreePanel actions for keys
+        return true;                            // Enable default TreePanel actions for keys
     },
 
     /**
      * Handle various key actions
      */
     onCellKeyDown: function(table, htmlTd, cellIndex, record, htmlTr, rowIndex, e, eOpts) {
-	var keyCode = e.getKey();
-	var keyCtrl = e.ctrlKey;
-	// console.log('GanttButtonController.onCellKeyDown: code='+keyCode+', ctrl='+keyCtrl);
+        var keyCode = e.getKey();
+        var keyCtrl = e.ctrlKey;
+        // console.log('GanttButtonController.onCellKeyDown: code='+keyCode+', ctrl='+keyCtrl);
+    },
+
+
+    /**
+     * The windows as a whole was resized
+     */
+    onWindowsResize: function(width, height) {
+        console.log('GanttButtonController.onWindowResize');
+        var me = this;
+        var sideBar = Ext.get('sidebar');				// ]po[ left side bar component
+        var sideBarSize = sideBar.getSize();
+
+	me.onResize(sideBarSize.width);
+    },
+
+    /**
+     * The ]po[ left sideBar was resized
+     */
+    onSideBarResize: function(event, el, config) {
+        console.log('GanttButtonController.onSideBarResize');
+        var me = this;
+        var sideBar = Ext.get('sidebar');				// ]po[ left side bar component
+        var sideBarSize = sideBar.getSize();
+
+	// We get the event _before_ the sideBar has changed it's size.
+	// So we actually need to the the oposite of the sidebar size:
+	if (sideBarSize.width > 100) {
+	    sideBarSize.width = -5;
+	} else {
+	    sideBarSize.width = 245;
+	}
+
+	me.onResize(sideBarSize.width);
+    },
+
+    /**
+     * Generic resizing function, called with the target width of the sideBar
+     */
+    onResize: function(sideBarWidth) {
+        console.log('GanttButtonController.onResize: '+sideBarWidth);
+        var me = this;
+        var screenSize = Ext.getBody().getViewSize();
+        var height = me.ganttEditor.getSize().height;
+        var width = screenSize.width - sideBarWidth - 75;
+        me.ganttEditor.setSize(width, height);
     }
     
 });
