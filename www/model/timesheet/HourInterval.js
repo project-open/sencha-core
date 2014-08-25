749d7ec77de0166b71026812008545d29cf54829
Frank Bergmann <frank.bergmann@project-open.com>
2014-08-25 13:19:27 +0000

    - Sencha Core:   Debugging HourInterval stuff

--- www/model/timesheet/HourInterval.js
+++ www/model/timesheet/HourInterval.js
@@ -2,15 +2,23 @@ Ext.define('PO.model.timesheet.HourInterval', {
     extend: 'Ext.data.Model',
 	fields: [
 	    'id',					// Same as interval_id
+
+	    // Fields stored on the REST Back-end
 	    'interval_id',				// Unique ID taken from im_hours_seq
 	    'user_id',					// Who logged the hours?
 	    'project_id',				// On which project or task?
-	    'interval_start',				// Start of time interval (PostgreSQL timestamp format)
+	    {name: 'interval_start', convert: null},				// Start of time interval (PostgreSQL timestamp format)
 	    'interval_end',				// End od time interval (PostgreSQL timestamp format)
 	    'note',					// Comment for the logged hours
 	    'internal_note',				// Comment hidden from customers (rarely used)
 	    'activity_type_id',				// Type of activity (meeting, work, ... (customer definable))
-	    'material_id'				// Type of service provided during hours (rarely used)
+	    'material_id',				// Type of service provided during hours (rarely used)
+
+	    // Add-on fields for editing, but nor for storage.
+	    // These fields are kept in sync using store events.
+	    'interval_date',	     	     	   	// Date part of start- and end time
+	    'interval_start_time',			// Time part of start date
+	    'interval_end_time'				// Time part of end date
 	],
 	proxy: {
 	    type:		'rest',
--- www/store/timesheet/HourIntervalStore.js
+++ www/store/timesheet/HourIntervalStore.js
@@ -28,6 +28,41 @@ Ext.define('PO.store.timesheet.HourIntervalStore', {
 	    project_id: 0		// Needs to be overwritten by controller
 	},
 	reader: { type: 'json', root: 'data' }
+    },
+
+    listeners: {
+	update: function(store, record, operation, modifiedFields) { 
+	    if ("commit" == operation) { return; }
+	    // if (store.isLoading()) { return; }
+	    console.log('PO.controller.timesheet.HourIntervalStore: update: '+record + ', modified='+modifiedFields);
+	},
+
+	// Extract the editable fields interval_date, start_time and end_time
+	// from the data returned by the ]po[ REST interface.
+	load: function(store, records, successful, eOpts) {
+	    if (null == records) { return; }
+	    console.log('PO.controller.timesheet.HourIntervalStore: load: ');
+
+	    var regexp = /(\d\d:\d\d)/;
+	    for (var i = 0; i < records.length; i++) {
+		var rec = records[i];
+		var start = rec.get('interval_start');
+		var end = rec.get('interval_end');
+
+		// Update the data WITHOUT using rec.set(...)
+		rec.data['interval_date'] = start.substring(0,10);
+		var regArr = regexp.exec(start);
+		rec.data['interval_start_time'] = regArr[1];
+		var regArr = regexp.exec(end);
+		rec.data['interval_end_time'] = regArr[1];
+
+		store.afterEdit(rec, ['interval_date', 'interval_start_time', 'interval_end_time']);
+	    }
+
+	    var isLoading = store.isLoading();
+	    store.loading = true;
+	    store.loading = isLoading;
+	}
     }
 });
 
