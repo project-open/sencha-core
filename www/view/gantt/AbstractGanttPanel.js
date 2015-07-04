e3a15735577f66dbcc321a7602c1e1a605ad2ce9
Frank Bergmann <frank.bergmann@project-open.com>
2015-07-04 12:18:43 +0000

    - GanttEditor

--- www/view/gantt/AbstractGanttPanel.js
+++ www/view/gantt/AbstractGanttPanel.js
@@ -352,8 +352,8 @@ Ext.define('PO.view.gantt.AbstractGanttPanel', {
                     for (var v in model.data) { data[v] = model.data[v]; }
                     data['value'] = lastValue;
                     data['maxValue'] = maxGraphArray;
-                    data['startDate'] = segmentStartDate.toPg().substring(0,10);
-                    data['endDate'] = new Date(lastIntervalEndDate.getTime() - oneDayMilliseconds).toPg().substring(0,10);
+                    data['startDate'] = PO.Utilities.dateToPg(segmentStartDate).substring(0,10);
+                    data['endDate'] = PO.Utilities.dateToPg(new Date(lastIntervalEndDate.getTime() - oneDayMilliseconds)).substring(0,10);
                     var tip = Ext.create("Ext.tip.ToolTip", {
                         target: spritePath.el,
                         width: 250,
