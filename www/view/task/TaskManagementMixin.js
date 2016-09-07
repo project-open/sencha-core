3aaa86f45bd492e47c1684f68c42f7f20e997e83
Frank Bergmann <frank.bergmann@project-open.com>
2016-09-07 15:03:18 +0000

    - Task Management:   - Now showing ToolTip information for each task   - Changed naming

--- www/view/task/TaskManagementMixin.js
+++ www/view/task/TaskManagementMixin.js
@@ -13,14 +13,27 @@ Ext.define('PO.view.task.TaskManagementMixin', {
      */
     drawTaskBox: function(surface, fillColor, model, x, y, w, h) {
 	var colorCode = model.get('color_code');
+	var nameL10n = "Name", startL10n = "Start", endL10n = "End", doneL10n = "Done";
+
+	// Build a HTML table with information about the task/ticket/project
+        var html = "<table cellpadding=0 cellspacing=2>";
+	html = html + "<tr><td>" + nameL10n + ":</td><td><nobr>" + model.get('project_name') + "</nobr></td></tr>";
+	var startDate = model.get('start_date'); 
+	if (startDate) html = html + "<tr><td>" + startL10n + ":</td><td>" + startDate.substring(0,10) + "</td></tr>";
+	var endDate = model.get('end_date');
+	if (endDate) html = html + "<tr><td>" + endL10n + ":</td><td>" + endDate.substring(0,10) + "</td></tr>";
+	var percentCompleted = model.get('percent_completed');
+	if (100 == model.get('type_id') && ("" == percentCompleted || !percentCompleted)) { percentCompleted = 0 };
+	if (null != percentCompleted) html = html + "<tr><td>" + doneL10n + ":</td><td>" + percentCompleted + "%</td></tr>";
+	html = html + "</table>";
+
         var box = surface.add({
             type: 'rect', x: x, y: y, width: w, height: h, 
             fill: fillColor,
             stroke: 'black', 'stroke-width': 0.0
         }).show(true);
         box.project_id = model.get('project_id');                                    // set project_id for onclick event
-        var tooltip = model.get('project_name') + ' (' + colorCode + ')';
-        Ext.create("Ext.tip.ToolTip", { target: box.el, html: tooltip, hideDelay: 1000 });
+        Ext.create("Ext.tip.ToolTip", { target: box.el, html: html, hideDelay: 1000 });
         box.on({
             click: function(sprite, event, fn) {
                 var pid = this.project_id;                                           // this is the project_id added above
