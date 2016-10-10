45778521c356361dea8e64bb4aafedd370770005
Frank Bergmann <frank.bergmann@project-open.com>
2016-10-10 10:49:17 +0000

    - Removed pageUrl from set* function parameters   and replaced with the URL from the browser. - Added consistent preference getters for int and   boolean

--- www/store/user/SenchaPreferenceStore.js
+++ www/store/user/SenchaPreferenceStore.js
@@ -6,10 +6,11 @@
  * http://www.project-open.com/license/sencha/ for details.
  *
  * <ul>
- * <li>A store with the list of all active main projects in the system.
- * <li>The store needs explicit sync() in order to store changes.
- * <li>The store does not explicitely exclude tasks and tickets
- *     with parent_id=NULL, which may occur accidentally in ]po[.
+ * <li>A store for key-value data per URL/page.
+ * <li>All values are converted to string on the database
+ *     level. However, we provide access functions for 
+ *     integer and boolean if the values are know to belong
+ *     to these types.
  * </ul>
  */
 
@@ -22,23 +23,21 @@ Ext.define('PO.store.user.SenchaPreferenceStore', {
     remoteFilter:	true,					// Do not filter on the Sencha side
     pageSize:		100000,					// Load all projects, no matter what size(?)
     proxy: {
-	type:		'rest',					// Standard ]po[ REST interface for loading
-	url:		'/intranet-rest/im_sencha_preference',
-	appendId:	true,
-	timeout:	300000,
-	extraParams: {
-	    format:		'json'
-	    // deref_p:		'1'				// We don't need company_name etc.
-	    // This should be overwrittten during load.
-	},
-	reader: {
-	    type:		'json',				// Tell the Proxy Reader to parse JSON
-	    root:		'data',				// Where do the data start in the JSON file?
-	    totalProperty:	'total'				// Total number of tickets for pagination
-	},
-	writer: {
-	    type:		'json'				// Allow Sencha to write ticket changes
-	}
+        type:		'rest',					// Standard ]po[ REST interface for loading
+        url:		'/intranet-rest/im_sencha_preference',
+        appendId:	true,
+        timeout:	300000,
+        extraParams: {
+            format:		'json'
+        },
+        reader: {
+            type:		'json',				// Tell the Proxy Reader to parse JSON
+            root:		'data',				// Where do the data start in the JSON file?
+            totalProperty:	'total'				// Total number of tickets for pagination
+        },
+        writer: {
+            type:		'json'				// Allow Sencha to write ticket changes
+        }
     },
 
     /**
@@ -46,40 +45,45 @@ Ext.define('PO.store.user.SenchaPreferenceStore', {
      * an already existing preference or creating a new one.
      * The preference is written to the server REST back-end.
      */
-    setPreference: function(preferenceUrl, preferenceKey, preferenceValue) {
-	var prefModel = this.findRecord('preference_key',preferenceKey);
-	if (null == prefModel) {
-	    // We need to create a new preference
+    setPreference: function(preferenceKey, preferenceValue) {
+	var preferenceUrl = window.location.pathname;
+        var prefModel = this.findRecord('preference_key',preferenceKey);
+        if (null == prefModel) {
+            // We need to create a new preference
             prefModel = Ext.create('PO.model.user.SenchaPreference', {
-		preference_url: preferenceUrl,                 // URL = "section" of parameters
-		preference_key: preferenceKey,                 // Use the element's ID as key for the true/false preference
-		preference_value: ''+preferenceValue           // The REST Back-end only works with strings
+                preference_url: preferenceUrl,         		// URL = "section" of parameters
+                preference_key: preferenceKey,         		// Use the element's ID as key for the true/false preference
+                preference_value: ''+preferenceValue   		// The REST Back-end only works with strings
             });
-            prefModel.save();                                       // POST the object to the REST API, creating a new object
-	    this.add(prefModel);
-	} else {
-	    // The preference already exists
-	    prefModel.set('preference_value', ''+preferenceValue);
-            prefModel.save();                                       // POST the object to the REST API, performing an update
-	}
-    },
-
-    /**
-     * Shortcut for getPrefereceString
-     */
-    getPreference: function(preferenceKey, defaultValue) {
-	return this.getPreferenceString(preferenceKey, defaultValue);
+            prefModel.save();                               	// POST the object to the REST API, creating a new object
+            this.add(prefModel);
+        } else {
+            // The preference already exists
+            prefModel.set('preference_value', ''+preferenceValue);
+            prefModel.save();                               	// POST the object to the REST API, performing an update
+        }
     },
 
     /**
      * Returns the specified preference as a boolean.
      */
+    getPreference: function(preferenceKey, defaultValue) {
+	var preferenceUrl = window.location.pathname;
+
+	// ToDo: deal with preferenceUrl!!!
+
+        var prefModel = this.findRecord('preference_key',preferenceKey);
+        if (null == prefModel) { return defaultValue; }
+        var prefValue = prefModel.get('preference_value');
+        if (null == prefValue) { return defaultValue; }
+        return prefValue;
+    },
+
+    /**
+     * Shortcut for getPreferece
+     */
     getPreferenceString: function(preferenceKey, defaultValue) {
-	var prefModel = this.findRecord('preference_key',preferenceKey);
-	if (null == prefModel) { return defaultValue; }
-	var prefValue = prefModel.get('preference_value');
-	if (null == prefValue) { return defaultValue; }
-	return prefValue;
+        return this.getPreference(preferenceKey, defaultValue);
     },
 
     /**
@@ -87,22 +91,31 @@ Ext.define('PO.store.user.SenchaPreferenceStore', {
      * False is the default value for the default value.
      */
     getPreferenceBoolean: function(preferenceKey, defaultValue) {
-	if (null == defaultValue) { defaultValue = false; }
-	var prefModel = this.findRecord('preference_key',preferenceKey);
-	if (null == prefModel) { return defaultValue; }
-	var prefValue = prefModel.get('preference_value');
-	if (null == prefValue) { return defaultValue; }
-	var prefResult = prefValue == 'true' ? true : false;
-	return prefResult;
+        if (null == defaultValue) { defaultValue = false; }
+	var prefValue = this.getPreferenceString(preferenceKey, defaultValue);
+        var prefResult = prefValue == 'true' ? true : false;
+        return prefResult;
+    },
+
+    /**
+     * Returns the specified preference as an integer.
+     */
+    getPreferenceInt: function(preferenceKey, defaultValue) {
+	var prefValue = this.getPreferenceString(preferenceKey, defaultValue);
+        return parseInt(prefValue);
     },
 
     /**
      * Check if a key exists
      */
     existsPreference: function(preferenceKey) {
-	var prefModel = this.findRecord('preference_key',preferenceKey);
-	return (null != prefModel);
-    },
+	var preferenceUrl = window.location.pathname;
+
+	// ToDo: deal with preferenceUrl!!!
+
+        var prefModel = this.findRecord('preference_key',preferenceKey);
+        return (null != prefModel);
+    }
 
 });
 
