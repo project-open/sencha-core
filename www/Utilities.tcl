a9cf03bf79df8e4592cbd9cd7667389304a76aea
cosine <>
2018-01-01 14:34:09 +0000

    - Removed deprecated proc

--- www/Utilities.tcl
+++ www/Utilities.tcl
@@ -3,7 +3,12 @@
 ns_set cput [ns_conn outputheaders] "Access-Control-Allow-Origin" "*"
 
 set user_id [auth::require_login]
-set error_location "[ns_info address] on [ns_info platform]"
+
+set platform $::tcl_platform(platform)
+set os $::tcl_platform(os)
+set os_version $::tcl_platform(osVersion)
+
+set error_location "[ns_info address] on $platform"
 set report_url [parameter::get -package_id [im_package_core_id] -parameter "ErrorReportURL" -default ""]
 set error_type "default"
 set system_url [parameter::get -package_id [ad_acs_kernel_id] -parameter SystemURL -default ""]
@@ -16,10 +21,6 @@ set publisher_name [parameter::get -package_id [ad_acs_kernel_id] -parameter Pub
 set package_versions [db_list package_versions "select v.package_key||':'||v.version_name from (select max(version_id) as version_id, package_key from apm_package_versions group by package_key) m, apm_package_versions v where m.version_id = v.version_id"]
 set system_id [im_system_id]
 set hardware_id [im_hardware_id]
-
-set platform $::tcl_platform(platform)
-set os $::tcl_platform(os)
-set os_version $::tcl_platform(osVersion)
 
 set sencha_version_tuples [im_sencha_extjs_version]
 set sencha_version [lindex $sencha_version_tuples 0]; # "v421" or similar
