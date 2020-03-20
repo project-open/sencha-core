7a637b4e0d9f2a1f86ee08f444863a23a2c618e1
Frank Bergmann <frank.bergmann@project-open.com>
2020-03-20 16:01:45 +0000

    - WIP, this is supposed to become a combo box for the "tasks match"   field in a budget, showing a drop-down box of all tasks in the project

---
+++ www/view/field/POComboTree.js
@@ -0,0 +1,59 @@
+/*
+ * POComboTree.js
+ *
+ * Copyright (c) 2011 - 2020 ]project-open[ Business Solutions, S.L.
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 or alternatively unter the terms of the ]po[
+ * FL or CL license as specified in www.project-open.com/en/license.
+ */
+
+/**
+ * A ComboBox that takes a tree definition for the picker.
+ * Here is a configuration example using the POComboTree
+ * as an Editor:
+ *
+ * editor: {
+ *     xtype: 'pocombotree',
+ *     store: Ext.data.StoreManager.lookup('taskStatusStore'),
+ *     queryMode: 'local',
+ *     matchFieldWidth: false,                   // Allow the picker to be larger than the field width
+ *     displayField: 'category',
+ *     valueField: 'category_id',
+ *     listConfig: {
+ *         columns: [
+ *             {header: 'ID', dataIndex: 'category_id'},
+ *             {header: 'Category', dataIndex: 'category'}
+ *                     ]
+ *     }
+ * }
+ */
+Ext.define('PO.view.field.POComboTree', {
+    extend: 'Ext.form.field.Picker',
+    requires: ['Ext.tree.Panel'],
+    alias: 'widget.pocombotree',
+
+    // copied from ComboBox 
+    createPicker: function() {
+        var me = this;
+        var menuCls = Ext.baseCSSPrefix + 'menu';
+        var opts = Ext.apply({
+            floating: true,
+            ownerCt: me.ownerCt,
+            cls: me.el.up('.' + menuCls) ? menuCls : '',
+            store: me.store
+        }, me.columnConfig);
+
+	// NOTE: we simply use a tree panel
+        //picker = me.picker = Ext.create('Ext.view.BoundList', opts);
+	var picker = me.picker = Ext.create('Ext.tree.Panel', opts);
+
+	// hack: pass getNode() to the view
+	picker.getNode = function() {
+	    picker.getView().getNode(arguments);
+	};
+
+        return picker;
+    }
+
+});
+
